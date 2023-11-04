4a27ababbf4ce8a0ba7269bfb41bcc88ac613b4e
U200709023 <tugcedogru3@posta.mu.edu.tr>
2023-11-04 13:35:51 +0300

    lab4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.tugcedogru.mytagram"
-    compileSdk = 34
+    namespace = "com.tugcedogru.mybrowser"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.tugcedogru.mytagram"
+        applicationId = "com.tugcedogru.mybrowser"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -33,8 +33,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Mytagram"
+rootProject.name = "MyBrowser"
 include(":app")
  
